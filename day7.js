0ff6372fd205fd8140dd236cc76f85df16512164
SINGA SANJEEVARAJU <sanjeevaraj123@gmail.com>
2024-10-27 23:16:03 +0530

    files are added

---
+++ day7.js
@@ -0,0 +1,35 @@
+var age=22;
+if(age>18){
+    console.log("eligible for vote");
+   } else{
+        console.log("not eligible")
+}
+
+var whether="rainy";
+if(whether=="rainy"){
+    console.log("its pouring outside");
+}else{
+console.log("not raining")
+}
+var marks =76
+if(marks>=90){
+    console.log("grade A+");
+}
+else if(marks>=81 && marks<90){
+    console.log("grade A");
+}
+else if(marks>=71  && marks<80){
+    console.log("grade B+");
+}
+else if(marks>=61 && marks<60){
+    console.log("grade C+");-v
+}
+else if(marks>=51 && marks<50){
+    console,log("grade C")
+}
+else if(marks>=40 && marks<50){
+    console.log("grade D")
+}
+else{
+    console.log("fail")
+}
